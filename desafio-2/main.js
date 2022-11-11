c79314748781e96fb2540a29bbc14a9576ec730f
Luciano Nunez <lucianonahuelnunez@gmail.com>
2022-11-11 19:38:28 -0300

    desafio 2 y 3 arreglos

--- desafio-2/main.js
+++ desafio-2/main.js
@@ -1,109 +1,57 @@
-const fs = require('fs');
-
-class Contenedor {
-    archivo
-    objetos
-    
-    constructor(rutaArchivo, id, title, price, thumbnail) {
-        this.archivo = rutaArchivo;
-        this.objetos = [{id: id, title: title, price: price, thumbnail: thumbnail}];
-    }
-    
-    async save(nuevoProducto) {
-        this.objetos.push(nuevoProducto);
-        await fs.promises.writeFile(this.archivo, JSON.stringify(this.objetos));
-        return "Producto agregado";
-    }
-
-    async getById(id) {
-        this.objetos = JSON.parse(await fs.promises.readFile(this.archivo, 'utf-8'))
-        const productoBuscado = this.objetos.find(elemento => elemento.id === id);
-        if(productoBuscado === undefined){
-            return null;
-        }
-        else{
-             return productoBuscado; 
-        }
-    }
-
-    async getAll() {
-        this.objetos = JSON.parse(await fs.promises.readFile(this.archivo, 'utf-8'))
-        return this.objetos
-    }
-
-    async deleteById(id) {
-        this.objetos = JSON.parse(await fs.promises.readFile(this.archivo, 'utf-8'))
-        for(let i = 0; i < this.objetos.length; i++){
-            if(this.objetos[i].id === id){
-                this.objetos.splice(i,1);
-            }
-        }
-        await fs.promises.writeFile(this.archivo, JSON.stringify(this.objetos));
-        return "Producto con id: "+ id + " eliminado";
-    }
-
-    async deleteAll() {
-    
-        this.objetos = []
-        await fs.promises.writeFile(this.archivo, JSON.stringify(this.objetos));
-    }
-}
-
-
+const Contenedor = require("./ContenedorArchivos")
 async function test() {
     const rutaArchivo = 'productos.txt'
-    await fs.promises.writeFile(rutaArchivo, JSON.stringify([]))
-    //./curso-backend/desafio-2/
-    //creo el contenedor
-    const producto = new Contenedor(rutaArchivo)
-    
+
+    // //creo el contenedor
+    const contenedorProductos = new Contenedor(rutaArchivo)
+
     //agrego productos
     const producto1 = {
         id: 1,
-        title:"cafe",
+        title: "cafe",
         price: 500,
         thumbnail: "url"
     }
     const producto2 = {
         id: 2,
-        title:"cocacola",
+        title: "cocacola",
         price: 250,
         thumbnail: "url"
     }
     const producto3 = {
         id: 3,
-        title:"pepsi",
+        title: "pepsi",
         price: 200,
         thumbnail: "url"
     }
     const producto4 = {
         id: 4,
-        title:"te",
+        title: "te",
         price: 100,
         thumbnail: "url"
     }
-    console.log(await producto.save(producto1))
-    console.log(await producto.save(producto2))
-    
-    //Busco los productos por su id
-    console.log(await producto.getById(1))
-    
-    //Elimino un producto por id
-    console.log(await producto.deleteById(2))
-    
-    //Busco todos los productos
-    console.log(await producto.getAll())
-    
+    console.log(await contenedorProductos.save(producto1))
+    console.log(await contenedorProductos.save(producto2))
+
+    // //Busco los productos por su id
+    // console.log(await producto.getById(1))
+
+    // //Elimino un producto por id
+    console.log(await contenedorProductos.deleteById(2))
+
+    // //Busco todos los productos
+    console.log(await contenedorProductos.getAll())
+
     //agrego mas productos
-    console.log(await producto.save(producto3))
-    console.log(await producto.save(producto4))
+    console.log(await contenedorProductos.save(producto3))
+    console.log(await contenedorProductos.save(producto4))
 
     //get all
-    console.log(await producto.getAll())
-    
-   //Elimino todos los productos
-    //await producto.deleteAll()
-    //console.log(await producto.getAll())
+    console.log(await contenedorProductos.getAll())
+
+    //Elimino todos los productos
+    // await contenedorProductos.deleteAll()
+    // console.log(await contenedorProductos.getAll())
 }
 
 test()
--- desafio-3/servidor.js
+++ desafio-3/servidor.js
@@ -2,57 +2,15 @@ const fs = require('fs');
 const express = require('express')
 const server = express()
 
-async function read() {
-    try {
-        const result = JSON.parse(await fs.promises.readFile(`./productos.txt`,'utf-8'))
-        return result
-    } catch (error) {
-        return(`Archivo no encontrado`)
-    }
-}
-
-class Contenedor {
-    archivo
-    
-    constructor(rutaArchivo) {
-        this.archivo = rutaArchivo;
-    }
-
-    async getRandom() {
-        let resultadoPorId
-        await read(this.archivo).then(result => {
-            if (result == `Archivo ${this.archivo} no encontrado`) {
-                resultadoPorId = result
-            } else {
-                let a = result.find(el => el.id === 1)
-                if (a) {
-                    resultadoPorId = a
-                } else {
-                    resultadoPorId = 'Producto no encontrado'
-                }
-            }
-        })
-        return resultadoPorId
-    }
-
-    async getAll() {
-        let resultadoTotal
-        await read(this.archivo).then(result => {
-            resultadoTotal = result
-        })
-        return resultadoTotal
-    }
-
-}
+const Contenedor = require("./ContenedorArchivos")
+const contenedor =  new Contenedor('productos.txt')
 
 
 server.get('/productos', async (peticion, respuesta) => {
-    const contenedor =  new Contenedor('productos')
     respuesta.send(await contenedor.getAll()) 
 })
 
 server.get('/productoRandom', async (peticion, respuesta) => {
-    const contenedor =  new Contenedor('productos')
     respuesta.send(await contenedor.getRandom()) 
 })
 
