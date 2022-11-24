59fd651004458d5fdb962e45717a23acfe1191da
Luciano Nunez <lucianonahuelnunez@gmail.com>
2022-11-23 22:09:44 -0300

    actualizacion

--- desafio-4/controllers/controllerProductos.js
+++ desafio-4/controllers/controllerProductos.js
@@ -15,8 +15,7 @@ async function controladorGetProductos(peticion, res){
 } 
 async function controladorGetProductosSegunId({params: {id}}, res){
     try {
-        productos = await contenedor.getAll()
-        const buscado = productos.find(c => c.id === id);
+        const buscado = await contenedor.getById(id);
         if (!buscado) {
             res.status(404);
             res.json({ mensaje: `no se encontró producto con el id (${id})` });
@@ -28,13 +27,13 @@ async function controladorGetProductosSegunId({params: {id}}, res){
         res.json({mensaje: "Error al mostrar el producto por id"})
     }
 } 
-async function controladorPostProductos(peticion, res){
+async function controladorPostProductos(req, res){
     try {
         const nuevoProducto = req.body;
         nuevoProducto.id = randomUUID();
-        await save(nuevoProducto);
+        await contenedor.save(nuevoProducto);
         res.status(201);
-        respuesta.json(nuevoProducto);
+        res.json(nuevoProducto);
     } catch (error) {
         res.status(500);
         res.json({mensaje: "Error al agregar un producto"})
@@ -42,25 +41,33 @@ async function controladorPostProductos(peticion, res){
 } 
 async function controladorPutProductosSegunId({body, params: {id}}, res) {
    try {
-    productos = await contenedor.getAll()
+    const productos = await contenedor.getAll();
     const indiceBuscado = productos.findIndex(c => c.id === id);
     if (indiceBuscado === -1) {
         res.status(404);
         res.json({ mensaje: `no se encontró producto con ese id (${id})` });
     } else {
+        body.id = id
         productos[indiceBuscado] = body;
+        contenedor.update(productos);
         res.json(body);
     }
    } catch (error) {
     res.status(500);
-        res.json({mensaje: "Error al cambiar producto"})
+    res.json({mensaje: "Error al cambiar producto"})
    }
 }
 async function controladorDeleteProductosSegunId({ params: { id } }, res) {
     try {
-        const borrados = await deleteById(id)
-        res.json(borrados);
-        
+        const productos = await contenedor.getAll();
+        const indiceBuscado = productos.findIndex(c => c.id === id);
+        if (indiceBuscado === -1) {
+            res.status(404);
+            res.json({ mensaje: `no se encontró producto con ese id (${id})` });
+        } else {
+            await contenedor.deleteById(id);
+            res.json({mensaje: "exito al eliminar el producto"});
+        }
     } catch (error) {
         res.status(500);
         res.json({mensaje: "Error al eliminar el producto"})
