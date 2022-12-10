19f7d5a0b4b9408944a7ff5d6153908d8dda2d5b
Luciano Nuñez <lucianonahuelnunez@gmail.com>
2022-12-10 15:26:38 -0300

    desafio 8

--- desafio-8/routers/webRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { controladorGetProductos, controladorPostProductos } = require("../controllers/controllerProductos");
-
-const webRouter = express.Router()
-
-webRouter.get('/productos', controladorGetProductos);
-webRouter.post('/productos', controladorPostProductos);
-
-module.exports = { webRouter }
